9a7117c55596b493005da139ac281aad0c653537
Pe4en1e <alex36254@gmail.com>
2026-01-29 10:04:00 +0300

    initial backend

---
+++ MonitorItBackend/src/main/java/me/pe4en1e/monitoritbackend/MonitorItBackendApplication.java
@@ -0,0 +1,13 @@
+package me.pe4en1e.monitoritbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MonitorItBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MonitorItBackendApplication.class, args);
+    }
+
+}
---
+++ MonitorItBackend/src/test/java/me/pe4en1e/monitoritbackend/MonitorItBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package me.pe4en1e.monitoritbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MonitorItBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
